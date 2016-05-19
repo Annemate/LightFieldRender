bbdf0e4769bb036f6f5c3d0de443ca2c708f6e54
Jjakupklweien <lol@tfy.gjgtyu>
2016-05-19 21:11:44 +0200

    choose random and eliminate frmo selection works (on it's own script)

---
+++ Assets/120CamMosaic/listTest.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class listTest : MonoBehaviour {
+	public List<int> test = new List<int>();
+
+	int RandomNumber;
+	// Use this for initialization
+	void Start () {
+		test.Add(1);
+		test.Add(2);
+		test.Add(3);
+		test.Add(4);
+		test.Add(5);
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		RandomNumber = (int)Random.Range(0.0f, (float)(test.Count - 1));
+
+		print("the random index is " + RandomNumber + " The number chosen is " + test[RandomNumber] + " ,The remaning numbers are as follows:");
+
+		test.RemoveAt(RandomNumber);
+		for(int i = 0; i < test.Count; i++){
+			print(test[i]);
+		}
+
+		if(test.Count == 0){
+			print("list is empty. Refilling the list");
+
+			test.Add(1);
+			test.Add(2);
+			test.Add(3);
+			test.Add(4);
+			test.Add(5);
+		}
+
+		print("End of frame");
+
+	}
+}
