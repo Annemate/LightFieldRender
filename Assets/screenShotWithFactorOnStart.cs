dce5540c7471de6108d00e5ac1a91f7991cb3ac3
Jákup Klein <lol@tfy.gjgtyu>
2016-07-05 20:41:15 +0200

    made scene for image (for the report) images added to sharelatex

--- Assets/screenShotWithFactorOnStart.cs
+++ Assets/screenShotWithFactorOnStart.cs
@@ -5,7 +5,7 @@ public class screenShotWithFactorOnStart : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Application.CaptureScreenshot("Screenshot.png",5);
+		Application.CaptureScreenshot("Screenshot.png",3);
 	}
 
 	// Update is called once per frame
