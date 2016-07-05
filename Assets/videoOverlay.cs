6140072e92b5c8238cf36374b8b0bdfe52ac2447
Jákup Klein <lol@tfy.gjgtyu>
2016-07-05 18:48:18 +0200

    video plays

--- Assets/videoOverlay.cs
+++ Assets/videoOverlay.cs
@@ -6,8 +6,17 @@ public class videoOverlay : MonoBehaviour {
 	public MovieTexture myMovie;
 	// Use this for initialization
 	void Start () {
+		myMovie.loop = true;
+		myMovie.Play();
 
 	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), myMovie);
+	}
+
+
 
 	// Update is called once per frame
 	void Update () {
