e285cd53d7e1a5057f60d033920229fb3900f6d1
Jákup Klein <lol@tfy.gjgtyu>
2016-07-05 19:44:46 +0200

    video overlay works (neeed the correct video)

--- Assets/videoOverlay.cs
+++ Assets/videoOverlay.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class videoOverlay : MonoBehaviour {
 
 	public MovieTexture myMovie;
+	private bool showOverlay = false;
+	private bool canCheckButton = true;
+	private float buttonCheckDelay = 1f;
 	// Use this for initialization
 	void Start () {
 		myMovie.loop = true;
@@ -13,13 +16,40 @@ public class videoOverlay : MonoBehaviour {
 
 	void OnGUI()
 	{
-		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), myMovie);
+		if(showOverlay){
+			GUI.DrawTexture(new Rect(20, 20,20+ Screen.width,20+ Screen.height), myMovie);
+		}
 	}
 
+	void StartOverlay(){
+		myMovie.Play();
+		showOverlay = true;
+
+	}
+
+	void StopOverlay(){
+		showOverlay = false;
+		myMovie.Stop();
+	}
 
 
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown("v") && canCheckButton){
+			StartCoroutine(CanCheckButtonDelay(buttonCheckDelay));
+			if(showOverlay){
+				StopOverlay();
+			}else{
+				StartOverlay();
+			}
+		}
+	}
+
+	IEnumerator CanCheckButtonDelay(float delay){
+		canCheckButton = false;
+		yield return new WaitForSeconds(delay);
+		canCheckButton = true;
 
 	}
+
 }
