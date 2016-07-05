22cb83c98fc1b15c464f8f06e655414b739540f2
Jákup Klein <lol@tfy.gjgtyu>
2016-07-05 18:24:36 +0200

    working on video overlay

---
+++ Assets/videoOverlay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class videoOverlay : MonoBehaviour {
+
+	public MovieTexture myMovie;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
