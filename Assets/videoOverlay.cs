0546861489d2ebc31e2d790b52d0180ec248fc35
Jákup Klein <lol@tfy.gjgtyu>
2016-07-15 07:39:28 +0200

    demo image overlay works

--- Assets/videoOverlay.cs
+++ Assets/videoOverlay.cs
@@ -3,33 +3,47 @@ using System.Collections;
 
 public class videoOverlay : MonoBehaviour {
 
-	public MovieTexture myMovie;
+	//public MovieTexture myMovie;
+	private Material material;
+	public Texture2D emptyOverlayImage;
+	public Texture2D overlayImage;
 	private bool showOverlay = false;
 	private bool canCheckButton = true;
 	private float buttonCheckDelay = 1f;
 	// Use this for initialization
 	void Start () {
-		myMovie.loop = true;
-		myMovie.Play();
-
+		//myMovie.loop = true;
+		//myMovie.Play();
+		material = new Material( Shader.Find("Custom/ScaleShader") );
 	}
 
 	void OnGUI()
 	{
 		if(showOverlay){
-			GUI.DrawTexture(new Rect(20, 20,20+ Screen.width,20+ Screen.height), myMovie);
+			//GUI.DrawTexture(new Rect(20, 20,20+ Screen.width,20+ Screen.height), myMovie);
+			GUI.DrawTexture(new Rect(40, 21, 1205,643), overlayImage);
+			//GUI.DrawTexture(new Rect(20, 20,20+ Screen.width,20+ Screen.height), overlayImage);
 		}
 	}
 
 	void StartOverlay(){
-		myMovie.Play();
+		//myMovie.Play();
 		showOverlay = true;
 
 	}
 
 	void StopOverlay(){
 		showOverlay = false;
-		myMovie.Stop();
+		//myMovie.Stop();
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		if(showOverlay){
+			material.SetTexture ("_SubImages", overlayImage);
+		}else{
+			material.SetTexture ("_SubImages", emptyOverlayImage);
+		}
 	}
 
 
