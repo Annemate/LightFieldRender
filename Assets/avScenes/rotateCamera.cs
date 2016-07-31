306cf3807cd228873839267d903162ef70d74b37
Jákup Klein <lol@tfy.gjgtyu>
2016-07-31 18:13:29 +0200

    more av stuff

--- Assets/avScenes/rotateCamera.cs
+++ Assets/avScenes/rotateCamera.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class rotateCamera : MonoBehaviour {
 
 	public GameObject childCamera;
+	public GameObject fromObject;
+	public GameObject toObject;
      Transform from;
      Transform CameraPosition;
     public Transform to;
+    float tmpTime;
     int state = 0;
     public float speed = 0.1F;
 
@@ -17,7 +20,8 @@ public class rotateCamera : MonoBehaviour {
     }
     void Update() {
     	if(childCamera.transform.position.y < 60 && state == 0){
-		    childCamera.transform.Translate(Vector3.back * Time.deltaTime * 8);
+    		childCamera.transform.position = Vector3.Lerp(fromObject.transform.position, toObject.transform.position, Time.time * speed * 8);
+		    //childCamera.transform.Translate(Vector3.back * Time.deltaTime * 8);
 	        transform.rotation = Quaternion.Lerp(from.rotation, to.rotation, Time.time * speed);
 	    }
 
@@ -26,9 +30,11 @@ public class rotateCamera : MonoBehaviour {
 		  //  childCamera.transform.Translate(Vector3.right * Time.deltaTime * 5);
 	    }
 
-	    if(childCamera.transform.position.y > 0 && state == 2){
-		    childCamera.transform.Translate(Vector3.forward * Time.deltaTime * 8);
-	        transform.rotation = Quaternion.Lerp(to.rotation, from.rotation, Time.time * speed);
+	    if(childCamera.transform.position.y > 1 && state == 2){
+		    childCamera.transform.position = Vector3.Lerp(from.position, fromObject.transform.position, Time.time * speed * 0.1f);
+
+		   // childCamera.transform.Translate(Vector3.forward * Time.deltaTime * 8);
+	        transform.rotation = Quaternion.Lerp(to.rotation, Quaternion.identity, (Time.time - tmpTime) * speed);
 	    }
 
         if(Input.GetKeyUp("space") && state == 0){
@@ -37,7 +43,9 @@ public class rotateCamera : MonoBehaviour {
 
 		if(Input.GetKeyUp("a") && state == 1){
 			state = 2;
-
+			to = from;
+			from = childCamera.transform;
+			tmpTime = Time.time;
 		}
     }
 }
---
+++ Assets/avScenes/turnStuffOn.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class turnStuffOn : MonoBehaviour {
+
+	public List<GameObject> Lenses;
+	public List<GameObject> Lines;
+	public List<GameObject> Cameras;
+	public List<GameObject> Images;
+	bool lens;
+	bool line;
+	bool camera;
+	bool image;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyUp("a")){
+			lens = !lens;
+			for(int i = 0; i < Lenses.Count; i ++){
+				Lenses[i].SetActive(lens);
+			}
+		}
+
+		if(Input.GetKeyUp("b")){
+			line = !line;
+			for(int i = 0; i < Lines.Count; i ++){
+				Lines[i].SetActive(line);
+			}
+		}
+
+		if(Input.GetKeyUp("c")){
+			camera = !camera;
+			for(int i = 0; i < Cameras.Count; i ++){
+				Cameras[i].SetActive(camera);
+			}
+		}
+
+		if(Input.GetKeyUp("d")){
+			image = !image;
+			for(int i = 0; i < Images.Count; i ++){
+				Images[i].SetActive(image);
+			}
+		}
+	}
+}
