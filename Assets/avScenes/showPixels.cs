17207a33b372dac61be8e9d248e24b4348e617b9
Jákup Klein <lol@tfy.gjgtyu>
2016-08-01 16:21:38 +0200

    shader av thing

--- Assets/avScenes/showPixels.cs
+++ Assets/avScenes/showPixels.cs
@@ -9,9 +9,15 @@ public class showPixels : MonoBehaviour {
 	public float interCameraDelay;
 	int activationCounter;
 	public List<GameObject> Pixels;
+	public GameObject realPixel;
 	// Use this for initialization
 	void Start () {
+		realPixel.GetComponent<Renderer>().material.color = Color.green;
 		state = true;
+		for(int i = 0; i < Pixels.Count; i++){
+				Pixels[i].SetActive(false);
+				//Pixels[i].transform.position += new Vector3(0f,0.1f*i,0f);
+		}
 	}
 
 	// Update is called once per frame
@@ -20,35 +26,26 @@ public class showPixels : MonoBehaviour {
 		if(Input.GetKeyUp("b")){
 			state = true;
 			initialdelay = Time.time;
+
 		}
 
 		if(state){
 			if(Time.time > (interCameraDelay * activationCounter) + initialdelay){
-				Pixels[activationCounter + 0].GetComponent<Renderer>().material.color = Color.black;
-				Pixels[activationCounter + 1].GetComponent<Renderer>().material.color = Color.black;
-				Pixels[activationCounter + 2].GetComponent<Renderer>().material.color = Color.black;
-				Pixels[activationCounter + 3].GetComponent<Renderer>().material.color = Color.black;
 
-				Pixels[activationCounter + 0].SetActive(true);
-				Pixels[activationCounter + 1].SetActive(true);
-				Pixels[activationCounter + 2].SetActive(true);
-				Pixels[activationCounter + 3].SetActive(true);
+				Pixels[activationCounter].GetComponent<Renderer>().material.color = Color.black;
+				Pixels[activationCounter].SetActive(true);
 
-
-
-				if(activationCounter > 0){
-					//Pixels[activationCounter - 1].GetComponent<Renderer>().material.color = Color.red;
-					//Pixels[activationCounter - 2].GetComponent<Renderer>().material.color = Color.red;
-					//Pixels[activationCounter - 3].GetComponent<Renderer>().material.color = Color.red;
-					//Pixels[activationCounter - 4].GetComponent<Renderer>().material.color = Color.red;
-
+				if(activationCounter == Pixels.Count - 1){
+					Pixels[activationCounter].GetComponent<Renderer>().material.color = Color.green;
+					state = false;
+					Pixels[activationCounter - 1].GetComponent<Renderer>().material.color = Color.red;
+				}
+				else if(activationCounter > 0){
+					Pixels[activationCounter - 1].GetComponent<Renderer>().material.color = Color.red;
 
 				}
 
-
-				activationCounter += 4;
-
-
+				activationCounter ++;
 
 			}
 		}
