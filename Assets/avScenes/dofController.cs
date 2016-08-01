40634c9d835d7d6e81482c4e03e15dfc1dc66e2b
Jákup Klein <lol@tfy.gjgtyu>
2016-08-01 17:28:01 +0200

    added dof and changed colors and textures

---
+++ Assets/avScenes/dofController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dofController : MonoBehaviour {
+
+	//DepthOfField dofScript;
+	float speed;
+	// Use this for initialization
+	void Start () {
+		//dofScript = gameObject.GetComponent<DepthOfField>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//dofScript.maxBlurSize -= Time.deltaTime * speed;
+	}
+}
