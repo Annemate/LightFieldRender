e598b6ea5965453508491a275d6e5a3a3b860d75
Annemate <bybub@h440.aau1x.klient5.nordkraft.site.aau.dk>
2016-08-01 12:39:20 +0200

    box

--- rData/tTest.R
+++ rData/tTest.R
@@ -1,8 +1,8 @@
-VC <- as.numeric(read.table("C:/Users/jakupklein/Desktop/VC.txt", header = FALSE))
-IC <- as.numeric(read.table("C:/Users/jakupklein/Desktop/IC.txt", header = FALSE))
+VC <- as.numeric(read.table("/Users/bybub/LightFieldRender/rData/VC.txt", header = FALSE))
+IC <- as.numeric(read.table("/Users/bybub/LightFieldRender/rData/IC.txt", header = FALSE))
 
 sd(VC, na.rm = FALSE)
-sd(VC, na.rm = FALSE)
+sd(IC, na.rm = FALSE)
 mean(VC)
 mean(IC)
 t.test(VC,IC, var.equal=TRUE, paired=FALSE)
